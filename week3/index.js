cb6e029595f17d8e04ee6b3c4527ae113b0e7d9b
Fachminur1 <fachminur2836@gmail.com>
2023-11-02 21:06:01 +0700

    module 2 task

---
+++ week3/index.js
@@ -0,0 +1,31 @@
+function moraine_lake() {
+        window.open("https://en.wikipedia.org/wiki/Moraine_Lake")
+      }
+
+function salar_de_uyuni() {
+        window.open("https://en.wikipedia.org/wiki/Salar_de_Uyuni")
+      }
+
+function santorini_island() {
+        window.open("https://en.wikipedia.org/wiki/Santorini")
+      }
+
+function angkor_wat() {
+        window.open("https://id.wikipedia.org/wiki/Angkor_Wat")
+      }
+
+function great_barrier_reef() {
+        window.open("https://en.wikipedia.org/wiki/Great_Barrier_Reef")
+      }
+
+      function home() {
+        window.open("index.html")
+      }
+
+      function journey() {
+        window.open("index2.html")
+      }
+
+      function community() {
+        window.open("index2.html")
+      }
---
+++ week3/readme.md
@@ -0,0 +1,18 @@
+# Assignment 2
+
+> Here is the information about the assignment 2 held by RevoU
+
+
+
+#### 1. Assignment purpose
+The assignment purpose is to test the knowledge of creating a responsive web.
+
+
+
+#### 2. Assignment deadline
+
+The deadline of this assignment is on Friday, 27th of October 2023. 23:25 GMT+7
+
+#### 3. The website
+
+https://comfy-parfait-0ead23.netlify.app/
